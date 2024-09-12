ebb36e65193bd5b79d35945104ffa49a95b472e7
Benedek Balázs <benbal87@gmail.com>
2024-09-12 19:18:03 +0200

    :tada: project initialized

---
+++ src/main/java/com/commsignia/vehiclemonitorappbe/VehicleMonitorAppBeApplication.java
@@ -0,0 +1,13 @@
+package com.commsignia.vehiclemonitorappbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleMonitorAppBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleMonitorAppBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/commsignia/vehiclemonitorappbe/VehicleMonitorAppBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commsignia.vehiclemonitorappbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleMonitorAppBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
